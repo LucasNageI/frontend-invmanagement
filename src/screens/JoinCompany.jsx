8ca5bcd62e72f24188d78558b87ba01aee950913
LucasNagel <lucasanagel@gmail.com>
2024-12-09 22:41:37 -0300

    bugfix: imports

--- src/screens/JoinCompany.jsx
+++ src/screens/JoinCompany.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import "../styles/screen_styles/JoinCompany.css"
-import { verifyBusinessKey, getAuthToken } from "../utils"
+import { verifyBusinessKey } from "../utils"
+import getAuthToken from "../utils/getAuthToken.js"
 
 const JoinCompany = () => {
   const [errorMessage, setErrorMessage] = useState("")
